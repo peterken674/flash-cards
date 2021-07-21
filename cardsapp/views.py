cba5fc1755f994af8ac25458648162312b1d6617
gabriel <ayuelgarang105@gmail.com>
2021-07-22 00:50:38 +0300

    Added classes and requests inside views file.

--- cardsapp/views.py
+++ cardsapp/views.py
@@ -1,101 +1,90 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.core.exceptions import *
-from .forms import SignUpForm, AddDeckForm, AddCardForm
-from .models import Deck, Card
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
-from django.utils import timezone
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Profile, FlashCard, Note
+from .serializers import ProfileSerializer, FlashcardSerializer, NoteSerializer
 
-@login_required(login_url=('signup/'))
-def homepage(request):
-    decks = Deck.objects.filter(user_id=request.user.id)
-    return render(request, 'homepage.html', {"decks": decks})
 
-def deck(request, deck):
-    current_deck = Deck.objects.get(id=deck)
-    cards = Card.objects.filter(deck_id=deck)
-    return render(request, 'deck.html', {"deck": current_deck, "cards": cards})
-
-def add_deck(request):
-    if request.method == 'POST':
-        form = AddDeckForm(request.POST)
-        if form.is_valid():
-            deck = Deck(title=form.cleaned_data.get('title'),
-                        user=request.user)
-            deck.save()
-            return redirect('homepage')
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
         else:
-            return render(request, 'add-deck.html', {'form': form})
-    else:
-        form = AddDeckForm()
-        return render(request, 'add-deck.html', {'form': form})
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-def add_card(request, deck):
-    if request.method == 'POST':
-        form = AddCardForm(request.POST)
-        if form.is_valid():
-            card = Card(title=form.cleaned_data.get('title'),
-                        notes=form.cleaned_data.get('notes'),
-                        deck=Deck.objects.get(id=deck))
-            card.save()
-            return redirect('deck', deck=int(deck))
+
+class FlashcardList(APIView):
+    def get(self, request, format=None):
+        all_flashcards = FlashCard.objects.all()
+        serializers = ProfileSerializer(all_flashcards, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = FlashcardSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk, format=None):
+        flashcard = self.get_flashcard(pk)
+        serializers = FlashcardSerializer(flashcard, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
         else:
-            return render(request, 'add-card.html', {'form': form})
-    else:
-        form = AddCardForm()
-        return render(request, 'add-card.html', {'form': form})
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        flashcard = self.get_flashcard(pk)
+        flashcard.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('homepage')
-    if request.method == 'POST':
-        form = SignUpForm(request.POST, request.FILES)
-        if form.is_valid():
-            print("handling signup")
-            user = form.save()
-            user.refresh_from_db()
-            user.profile.email = form.cleaned_data.get('email')
-            user.profile.profile_photo = form.cleaned_data.get('profile_photo')
-            user.profile.bio = form.cleaned_data.get('bio')
-            user.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            print("signed up")
-            return redirect('homepage')
+
+class NoteList(APIView):
+    def get(self, request, format=None):
+        all_notes = Note.objects.all()
+        serializers = ProfileSerializer(all_notes, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = NoteSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk, format=None):
+        note = self.get_note(pk)
+        serializers = NoteSerializer(note, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
         else:
-            return render(request, 'registration/signup.html', {'form': form})
-    else:
-        form = SignUpForm()
-        return render(request, 'registration/signup.html', {'form': form})
-
-
-def delete_view(request, card):
-    current_card=Card.objects.get(id=card)
-    deck=current_card.deck
-    current_card.delete()
-    return redirect('deck', deck=deck.id)
-
-def edit_card(request, card):
-    page_title = "Edit Card"
-    editting_card = Card.objects.get(id=card)
-
-    if request.method == 'POST':
-        form = AddCardForm(request.POST)
-        if form.is_valid():
-            card = Card.objects.get(id=card)
-            card.title=form.cleaned_data.get('title')
-            card.notes=form.cleaned_data.get('notes')
-            card.date_updated = timezone.now()
-            card.save()
-            return redirect('deck', deck=card.deck.id)
-        else:
-            return render(request, 'add-card.html', {'form': form, "page_title": page_title})
-    else:
-        form = AddCardForm(initial={'title': editting_card.title, 'notes': editting_card.notes})
-        return render(request, 'add-card.html', {'form': form, "page_title": page_title})
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        note = self.get_note(pk)
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
