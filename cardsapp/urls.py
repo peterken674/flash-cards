1c5581c1d650e8f36a4c3f67d224110b77aa1c71
gabriel <ayuelgarang105@gmail.com>
2021-07-22 01:02:16 +0300

    Create urls with pks

--- cardsapp/urls.py
+++ cardsapp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^api/notes/', views.NoteList.as_view()),
     url(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProfileList.as_view()),
     url(r'api/flashcards/flashcard-id/(?P<pk>[0-9]+)/$',views.flashcardList.as_view()),
-    url(r'api/notes/notes-id/(?P<pk>[0-9]+)/$',views.ProfileList.as_view())...
+    url(r'api/notes/notes-id/(?P<pk>[0-9]+)/$',views.ProfileList.as_view())
 ]
