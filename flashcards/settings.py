996664758d4bb26f8b667a2d547ab345050abea8
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-22 15:42:37 +0300

    Remove redundant database reference.

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -108,14 +108,6 @@ WSGI_APPLICATION = 'flashcards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flashcards',
-        'USER': 'peterken',
-        'PASSWORD': 'pa55'
-    }
-}
 
 
 # Password validation
